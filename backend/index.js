5bf81099330592988ba8d81f227c46642226cd50
Prince Kumar <you@example.com>
2025-04-04 23:59:41 +0530

    Port Changes

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 connect();
-const port = process.env.PORT ||5000;
+const port = process.env.PORT ||6000;
 
 app.listen(port,()=>{
     console.log(`Server is running on port ${port}`);
